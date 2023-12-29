a7e1058fb4fa4552da6229f7c1e14f088456f9e5
sumaaanth <sumanththota7@gmail.com>
2023-12-29 11:35:06 -0500

    backtracking

---
+++ Backtracking.java
@@ -0,0 +1,46 @@
+public class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    public TreeNode(int val){
+        this.val = val;
+    }
+
+}
+public class TreeMaze {
+    public boolean canReachLeaf(TreeNode root){
+        if(root == null || root.val == 0){
+            return false;
+        }
+        if(root.left == null && root.right == null){
+            return true;
+        }
+        if(canReachLeaf(root.left)){
+            return true;
+        }
+        if(canReachLeaf(root.right)){
+            return true;
+        }
+        return false;
+    }
+    public boolean leafPath(TreeNode root, ArrayList<Integer> path){
+        if(root == null || root.val == 0){
+            return false;
+        }
+        path.add(root.val);
+        if(root.left ==null || root.right == null){
+            return true;
+        }
+        if(leafPath(root.left, path)){
+            return true;
+        }
+        if(leafPath(root.right, path)){
+            return true;
+        }
+        path.remove(path.size() - 1);
+        return false;
+
+    }
+
+    
+}
